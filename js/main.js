6514fee8b87c7e452b02f8503cb51cea70e34605
mdfizar <136453073+mdfizar@users.noreply.github.com>
2023-10-04 10:07:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".mySwiper", { slidesPerView: "1",
+spaceBetween: 0,
+autoplay: {
+  delay: 2500,
+  disableOnInteraction: false,
+},
+pagination: {
+  el: ".swiper-pagination",
+  clickable: true,
+},
+navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
